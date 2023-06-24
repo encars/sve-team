0581867602815098e838cd8829ce6307ed622300
Enric <enric.carstens@gmail.com>
2023-06-24 14:25:20 +0200

    playerlist

--- components/PlayerList.tsx
+++ components/PlayerList.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { Match, User } from "@prisma/client";
+import { FaBullseye, FaQuestion } from "react-icons/fa";
+import { GiShield, GiStrikingBalls } from "react-icons/gi";
+
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./ui/accordion";
 import getPlayersByMatchId from "@/actions/getPlayersByMatchId";
 import UserAvatar from "./UserAvatar";
@@ -24,16 +27,19 @@ const PlayerList: React.FC<PlayerListProps> = async ({
                 <AccordionContent>
                     <div className="flex flex-col space-y-1">
                         {players.map((player: User) => (
-                            <div key={player.id} className="flex items-center justify-around gap-2 p-2 capitalize bg-primary rounded-md">
+                            <div key={player.id} className="flex items-center justify-around gap-2 px-4 py-2 capitalize bg-primary rounded-md">
                                 <UserAvatar user={player} />
-                                <p className="text-primary-foreground">
+                                <p className="flex-grow text-primary-foreground">
                                     {player.name}
                                 </p>
                                 <p className="text-primary-foreground">
-                                    {player.position}
+                                    {player.position === 'FORWARD' && <GiStrikingBalls size={24} />}
+                                    {player.position === 'CENTER' && <FaBullseye size={24} />}
+                                    {player.position === 'DEFENDER' && <GiShield size={24} />}
+                                    {player.position === null && <FaQuestion size={24} />}
                                 </p>
                             </div>
-                        ))}   
+                        ))}
                     </div>
                 </AccordionContent>
             </AccordionItem>
