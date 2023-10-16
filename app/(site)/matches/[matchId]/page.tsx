4c70eb00e7a5c981d14c5d1f446cc5b8300b46ae
Enric <enric.carstens@gmail.com>
2023-10-16 17:22:41 +0200

    fix new user fetching

--- app/(site)/matches/[matchId]/page.tsx
+++ app/(site)/matches/[matchId]/page.tsx
@@ -1,10 +1,13 @@
 import getMatchById from "@/actions/getMatchById";
 import getPlayersByMatchId from "@/actions/getPlayersByMatchId";
+import getUser from "@/actions/getUser";
 import MatchDetail from "@/components/MatchDetail";
 import { Match } from "@prisma/client";
 import { notFound } from "next/navigation";
 
 const MatchPage = async ({ params }: { params: { matchId: string } }) => {
+    const user = await getUser();
+
     const match: Match | null = await getMatchById(params.matchId);
     const players = await getPlayersByMatchId(params.matchId);
 
@@ -14,7 +17,7 @@ const MatchPage = async ({ params }: { params: { matchId: string } }) => {
 
     return (
         <main className="pt-14 h-screen bg-primary overflow-y-auto flex flex-col">
-            <MatchDetail match={match} players={players} currentUser={currentUser!} />
+            <MatchDetail match={match} players={players} currentUser={user!} />
         </main>
     )
 }
--- app/(site)/practices/[practiceId]/page.tsx
+++ app/(site)/practices/[practiceId]/page.tsx
@@ -1,10 +1,13 @@
 import getPlayersByPracticeId from "@/actions/getPlayersByPracticeId";
 import getPracticeById from "@/actions/getPracticeById";
+import getUser from "@/actions/getUser";
 import PracticeDetail from "@/components/PracticeDetail";
 import { Practice } from "@prisma/client";
 import { notFound } from "next/navigation";
 
 const PracticePage = async ({ params }: { params: { practiceId: string } }) => {
+    const user = await getUser();
+
     const practice: Practice | null = await getPracticeById(params.practiceId);
     const players = await getPlayersByPracticeId(params.practiceId)
 
@@ -14,7 +17,7 @@ const PracticePage = async ({ params }: { params: { practiceId: string } }) => {
 
     return (
         <main className="pt-16 h-screen bg-primary p-2">
-            <PracticeDetail practice={practice} players={players} currentUser={currentUser!} />
+            <PracticeDetail practice={practice} players={players} currentUser={user!} />
         </main>
     )
 }
